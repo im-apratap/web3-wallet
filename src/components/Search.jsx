ed1722e1bbfd43818132335a53a9c787b7fb6a15
Aaditya Pratap <aadityapratapiit@gmail.com>
2026-01-05 20:49:04 +0530

    Improved Search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Search = () => {
+  const [url, setUrl] = useState("");
 
-  const [url, setUrl] = useState("")
-
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="url-field" className='label-search'>Solana Blockchain Explorer</label>
+    <div className="search-container">
+      <h2 className="search-title">Solana Blockchain Explorer</h2>
+      <form onSubmit={handleSubmit} className="search-form">
         <input
-          type="url"
+          type="text"
           value={url}
           placeholder="Search By Solana's Public Key"
           onChange={(e) => setUrl(e.target.value)}
-          className='input-search'
+          className="input-search"
         />
         <button className="btn-primary">Search</button>
       </form>
     </div>
   );
-}
+};
 
-export default Search
+export default Search;
